875df8caa5765a7b9febf49cb5d49a7ec6d4eb14
AntimGurung99 <antimgurung314@gmail.com>
2026-03-05 02:56:52 +0800

    update registerpage styles

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -62,8 +62,13 @@ function RegisterPage(){
   
     return(
       <div className="top-register-page">
+        
           <div className="overlay">
+          
         <div className="register-form-container card--wide">
+          <div className="quote">
+            <p>Create your account and start exploring places across Nepal.</p>
+          </div>
 
           {error && (<div className="register-error">{error}</div>)}
 
@@ -72,74 +77,85 @@ function RegisterPage(){
 
 
         <div className="input-field-one">
-          <div className="input-field">
-        <input name="first_name"
-         value={Form.first_name} 
-         onChange={onChange}
-         placeholder="First Name"
-         required
-         />
+          
+              <input 
+              className="input"
+              name="first_name"
+               value={Form.first_name} 
+               onChange={onChange}
+               placeholder="First Name"
+               required
+              />
+       
 
-         </div>
-
-         <div className="input-field">
-        <input name="last_name"
+        <input 
+        className="input"
+        name="last_name"
          value={Form.last_name} 
          onChange={onChange} 
          placeholder="Last Name" 
          required
          />
          </div>
-         </div>
+         
 
 
-         <div className="input-field">
-        <input name="email"
+       
+        <input
+        className="input" 
+        name="email"
          value={Form.email} 
          onChange={onChange} 
          placeholder="Enter your email" 
          required/>
-        </div>
+        
 
-        <div className="input-field">
-        <input name="phone_number"
+       
+        <input 
+        className="input"
+        name="phone_number"
          value={Form.phone_number}
          onChange={onChange} 
-        placeholder="98xxxxxxxx" 
+        placeholder=" Phone Number" 
         maxLength={10}
         />
-       </div>
+       
  
         <div className="input-field-two"> 
-        <div className="input-field">
-        <input name="city"
+    
+        <input 
+        className="input"
+        name="city"
           value={Form.city}
           onChange={onChange} 
           placeholder="City"
           required
         />
-        </div>
         
-        <div className="input-field">
-        <input name="country" 
+        <input 
+        className="input"
+        name="country" 
           value={Form.country}  
           onChange={onChange} 
           placeholder="Country"
         />
         </div>
-        </div>
+      
 
-        <div className="input-field">
-        <input name="date_of_birth" 
+        
+        <input
+        className="input"
+         name="date_of_birth" 
             value={Form.date_of_birth} 
             onChange={onChange} type="date" 
             placeholder="Date of Birth (Optional)"
         />
-        </div>
+     
 
 
-        <div className="input-field">
+       
         <input
+        className="input"
         type="password"
         name="password"
         value={Form.password}
@@ -147,10 +163,10 @@ function RegisterPage(){
         placeholder="Password"
         required
         />
-        </div>
-
-       <div className="input-field">
+      
+    
         <input
+        className="input"
         type="password"
         name="confirm_password"
         value={Form.confirm_password}
@@ -158,7 +174,7 @@ function RegisterPage(){
         placeholder="Confirm Password"
         required
         />
-      </div>
+      
       </div>
 
         <label className="register-terms">
